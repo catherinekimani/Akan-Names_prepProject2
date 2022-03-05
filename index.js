e6bb62a4fff9dc77e984d31d0941346422828b13
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-05 17:43:26 +0300

    ft: Add multiple variables

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function findAkanName () {
-    
+//  gender  variables 
+    var male = document.getElementById("male").checked;
+    var female = document.getElementById("female").checked;
+
+    // date/month/year variables
+    var date = document.getElementById("date").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
+
+    // var weekdays
+    var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    // var Akan Names(males & females)
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    var femaleNames = ["Akosua", "Adwoa", "Abena", "Akuna", "Yaa", "Afua", "Ama"]
 }
