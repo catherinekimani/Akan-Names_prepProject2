c1ecdb292c64288d280f61e2ded6c9cf36147f00
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-06 09:21:32 +0300

    Add slight changes, fix bugs

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function findAkanName () {
-//  gender  variables 
-    var male = document.getElementById("male").checked;
-    var female = document.getElementById("female").checked;
+//  gender 
+    var gender = document.querySelector('input[name="gender"]:checked').value;
 
     // date/month/year variables
     var day = document.getElementById("day").value;
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
-
     // var weekdays
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
@@ -16,20 +14,19 @@ function findAkanName () {
     var femaleNames = ["Akosua", "Adwoa", "Abena", "Akuna", "Yaa", "Afua", "Ama"]
 
     // var date of birth
-    var dateOfBirth = new Date (day +'/' +month + '/' +year);
+    var dateOfBirth = new Date (year +'/' +month + '/' +day);
     var dayOfTheWeek = dateOfBirth.getDay();
 
-    // if else statement
+    // form validation
     if(day < 0 || day > 31) {
         document.getElementById("error-text").innerTextL="Enter Valid date!!!"
     } else if(year <= 0 || year > 2022){
         document.getElementById("error-text").innerText="Enter valid date!!!"
-    } else if(month >=0 || month < 12) {
-        Document.getElementById("error-text").innerText="Enter valid date!!!"
-    } else if (male === true) {
-       document.getElementById("showName").innerHTML="You were born on a " + days[dayOfTheWeek] + "." + " Your Akan name is " + maleNames[dayOfTheWeek] + "!"
-    } else if (female === true) {
-        Document.getElementById("showName").innerHTML="You were born on a " + days[dayOfTheWeek] + "." + " Your Akan name is " + femaleNames[dayOfTheWeek] + "!"
+    } else if(month <= 0 || month > 12) {
+        document.getElementById("error-text").innerText="Enter valid date!!!"
+    } else if (gender == "male") {
+       document.getElementById("showName").innerText="You were born on a " + days[dayOfTheWeek] + "." + " Your Akan name is " + maleNames[dayOfTheWeek] + "!"
+    } else if (gender == "female") {
+        document.getElementById("showName").innerText="You were born on a " + days[dayOfTheWeek] + "." + " Your Akan name is " + femaleNames[dayOfTheWeek] + "!"
     }
 }
-findAkanName();
